c8cf3e3e975dff8b21fc1962329a77d51020b1a0
Kamil Żogło <kamilzoglo@gmail.com>
2019-12-31 18:07:25 +0100

    Repair inside connect method(ssl option, needless port in connection string). Middlewares for all of the endpoints have been added.

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const express = require('express');
 const config = require('config');
 const cors = require('cors');
 const app = express();
+const users = require('./routes/users');
+const movies = require('./routes/movies');
+const rooms = require('./routes/rooms');
+const seats = require('./routes/seats');
+const screening = require('./routes/screening');
+const reservation = require('./routes/reservation');
 
 //routers
 
-
-  
 mongoose
-  .connect('mongodb://localhost:3000/CC5_Cinema', {
+  .connect('mongodb://localhost/CC5_Cinema', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    server: { ssl: true }
+    useUnifiedTopology: true
   })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
@@ -20,8 +23,12 @@ mongoose
 app.options('*', cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//app routers
+app.use('/api/users', users);
+app.use('/api/movies', movies);
+app.use('/api/rooms', rooms);
+app.use('/api/seats', seats);
+app.use('/api/screening', screening);
+app.use('/api/reservation', reservation);
 
 // require('./startup/prod')(app);
 
